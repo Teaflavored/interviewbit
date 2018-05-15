9a58a81618bf0d7b3caa8f90c92f984221d982e8
Auster Chen <auster.s.chen@gmail.com>
2018-05-14 20:38:38 -0700

    add concentricCircle

---
+++ concentricCircle.js
@@ -0,0 +1,21 @@
+function concentricCircle(A) {
+  if (A < 1) {
+    throw new Error('Invalid input');
+  }
+  
+  if (A === 1) {
+    return [[1]];
+  }
+  
+  const prev = concentricCircle(A - 1);
+  const prevN = prev[0].length;
+  const newRow = (new Array(prevN + 2)).fill(A);
+
+  return [
+    newRow,
+    ...(prev.map(prevRow => [A, ...prevRow, A])),
+    newRow,
+  ];
+}
+
+console.log(concentricCircle(4));
